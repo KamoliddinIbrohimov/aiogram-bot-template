6442f33122388d402a93f5b99cb9efae5d267f3e
KamoliddinIbrohimov <kamoliddinibrohimov777@gmail.com>
2022-11-24 22:42:05 +0500

    a lesson about handlers

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -1,9 +1,16 @@
 from aiogram import types
 
-from loader import dp
+from loader import dp, bot
 
 
 # Echo bot
 @dp.message_handler(state=None)
 async def bot_echo(message: types.Message):
+    """Send a message to the user"""
+    #method 1
     await message.answer(message.text)
+    #method 2
+    await message.reply(message.text)
+    #method 3
+    await bot.send_message(chat_id=message.chat.id, text=message.text)
+
